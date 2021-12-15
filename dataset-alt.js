232163fede468f43898c627125fc78cb6f2901e2
adamstein <adamjstein@gmail.com>
2021-12-15 17:23:04 +1000

    Add files via upload

---
+++ dataset-alt.js
@@ -0,0 +1,266 @@
+dataset = {
+    "records":
+    [
+        ////////////////////////////////////////
+        //microsoft
+        {   "cat": "microsoft", 
+            "text":"Azure", 
+            "icon":"fab fa-microsoft", 
+            "link":"https://portal.azure.com"
+        },
+        {   "cat": "microsoft", 
+            "text":"Sentinel", 
+            "icon":"fab fa-microsoft", 
+            "link":"https://portal.azure.com/#blade/Microsoft_Azure_Security_Insights/MainMenuBlade/0/subscriptionId/e472542b-3ba3-461b-9754-99ea000ff7df/resourceGroup/DPC_Sentinel_Prod_RG/workspaceName/DPCAzureSentinel"
+        },
+        {   "cat": "microsoft", 
+            "text":"Cloud App Security", 
+            "icon":"fab fa-microsoft", 
+            "link":"https://dpcqld.portal.cloudappsecurity.com"
+        },
+        {   "cat": "microsoft", 
+            "text":"Security & Compliance", 
+            "icon":"fab fa-microsoft", 
+            "link":"https://dpcqld.portal.cloudappsecurity.com"
+        },
+        {   "cat": "microsoft", 
+            "text":"Defender ATP", 
+            "icon":"fab fa-microsoft", 
+            "link":"https://securitycenter.windows.com/dashboard"
+        },
+
+
+
+
+        ////////////////////////////////////////
+        //news
+        {
+            "cat": "news",
+            "text": "Threatpost",
+            "icon": "fas fa-newspaper",
+            "link": "https://threatpost.com/"
+        },
+        {
+            "cat": "news",
+            "text": "Hacker News",
+            "icon": "fas fa-newspaper",
+            "link": "https://thehackernews.com/"
+        },
+        {
+            "cat": "news",
+            "text": "Feedly",
+            "icon": "fas fa-rss",
+            "link": "https://feedly.com/i/my"
+        },
+        {
+            "cat": "news",
+            "text": "news.com.au",
+            "icon": "fas fa-newspaper ",
+            "link": "https://www.news.com.au"
+        },
+        {
+            "cat": "news",
+            "text": "brisbanetimes.com.au",
+            "icon": "fas fa-newspaper ",
+            "link": "https://www.brisbanetimes.com.au/"
+        },
+        {
+            "cat": "news",
+            "text": "itnews.com.au",
+            "icon": "fas fa-newspaper ",
+            "link": "https://www.itnews.com.au/"
+        },
+        {
+            "cat": "news",
+            "text": "abc.net.au",
+            "icon": "fas fa-newspaper ",
+            "link": "https://www.abc.net.au/news"
+        },
+
+
+
+        ////////////////////////////////////////
+        //hunting
+        {
+            "cat": "hunting",
+            "text": "any.run",
+            "icon": "fas fa-key",
+            "link": "https://app.any.run/"
+        },
+        {
+            "cat": "hunting",
+            "text": "Virus Total",
+            "icon": "fas fa-spider",
+            "link": "https://www.virustotal.com/gui/home/upload"
+        },
+        {
+            "cat": "hunting",
+            "text": "urlscan.io",
+            "icon": "fas fa-search-location",
+            "link": "https://urlscan.io/"
+        },
+        {
+            "cat": "hunting",
+            "text": "hybrid-analysis",
+            "icon": "fas fa-search-location",
+            "link": "https://www.hybrid-analysis.com/"
+        },
+        {
+            "cat": "hunting",
+            "text": "Cyber Chef",
+            "icon": "fab fa-elementor",
+            "link": "https://gchq.github.io/CyberChef/"
+        },
+        {
+            "cat": "hunting",
+            "text": "Message Analyzer",
+            "icon": "fab fa-microsoft",
+            "link": "https://mha.azurewebsites.net/"
+        },
+        {
+            "cat": "hunting",
+            "text": "IP Info",
+            "icon": "fas fa-search-location",
+            "link": "https://ipinfo.io/"
+        },
+        {
+            "cat": "hunting",
+            "text": "ThreatCloud",
+            "icon": "fab fa-searchengin",
+            "link": "https://www.threatcrowd.org/"
+        },
+        {
+            "cat": "hunting",
+            "text": "Check short URL",
+            "icon": "fas fa-search",
+            "link": "http://www.checkshorturl.com/"
+        },
+        {
+            "cat": "hunting",
+            "text": "Uncoder.IO",
+            "icon": "fas fa-search",
+            "link": "https://uncoder.io/"
+        },
+        {
+            "cat": "hunting",
+            "text": "MITRE ATT&CK",
+            "icon": "",
+            "link": "https://attack.mitre.org/matrices/enterprise/"
+        },
+
+
+        ////////////////////////////////////////
+        //socials
+        {
+            "cat": "socials",
+            "text": "Twitter",
+            "icon": "fab fa-twitter  fa-2x'></i>",
+            "link": "https://twitter.com/"
+        },
+        {
+            "cat": "socials",
+            "text": "Reddit",
+            "icon": "fab fa-reddit-alien  fa-2x'></i>",
+            "link": "https://old.reddit.com/new"
+        },
+        {
+            "cat": "socials",
+            "text": "FaceBook",
+            "icon": "fab fa-facebook-f  fa-2x'></i>",
+            "link": "https://www.facebook.com"
+        },
+        {
+            "cat": "socials",
+            "text": "LinkedIn",
+            "icon": "fab fa-facebook-f  fa-2x'></i>",
+            "link": "https://www.linkedin.com.au"
+        },
+
+
+        ////////////////////////////////////////
+        //google
+        {
+            "cat": "google",
+            "text": "Messages",
+            "icon": "svgGmail",
+            "link": "https://messages.google.com/"
+        },
+        {
+            "cat": "google",
+            "text": "Gmail",
+            "icon": "svgGmail",
+            "link": "https://mail.google.com/mail/u/0/"
+        },
+        {
+            "cat": "google",
+            "text": "Calendar",
+            "icon": "svgCal",
+            "link": "https://calendar.google.com/calendar/r?pli=1"
+        },
+        {
+            "cat": "google",
+            "text": "Hangouts",
+            "icon": "svgHangouts",
+            "link": "https://hangouts.google.com/"
+        },
+        {
+            "cat": "google",
+            "text": "Photos",
+            "icon": "svgPhotos",
+            "link": "https://photos.google.com/"
+        },
+        {
+            "cat": "google",
+            "text": "Podcasts",
+            "icon": "svgPhotos",
+            "link": "https://podcasts.google.com/"
+        },
+        {
+            "cat": "google",
+            "text": "Drive",
+            "icon": "svgDrive",
+            "link": "https://drive.google.com/"
+        },
+        {
+            "cat": "google",
+            "text": "Keep",
+            "icon": "svgKeep",
+            "link": "https://keep.google.com/"
+        },
+
+        ////////////////////////////////////////
+        //learning
+        {
+            "cat": "learning",
+            "text": "Udemy",
+            "icon": "",
+            "link": "https://www.udemy.com/"
+        },
+        {
+            "cat": "learning",
+            "text": "awesome-security",
+            "icon": "",
+            "link": "https://github.com/sbilly/awesome-security"
+        },
+        {
+            "cat": "learning",
+            "text": "isc2 (cissp)",
+            "icon": "",
+            "link": "https://learn.isc2.org/d2l/home"
+        },
+        {
+            "cat": "learning",
+            "text": "pluralsight",
+            "icon": "",
+            "link": "https://app.pluralsight.com"
+        }
+
+
+
+
+
+
+
+
+
+]};
